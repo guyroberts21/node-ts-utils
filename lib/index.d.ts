aceff02f020c1622d6fed9abb3c444cca18a1070
Guy Roberts <guyroberts21@outlook.com>
2021-02-10 19:36:12 +0000

    Update gitignore

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const utils: (dataPath: string) => {
-    readFile: (callback: (returnJson: boolean) => any, returnJson?: boolean, filePath?: string, encoding?: string) => void;
-    writeFile: (fileData: string, callback: () => void, filePath?: string, encoding?: string) => void;
-};
