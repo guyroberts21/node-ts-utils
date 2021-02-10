21f0566fb475b20007a9050dc7eb594400510320
Guy Roberts <guyroberts21@outlook.com>
2021-02-10 19:38:18 +0000

    Fix indentation

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,38 @@
 import * as fs from 'fs';
 
 export const utils = (dataPath: string) => {
-	const readFile = (
-		callback: (returnJson: boolean) => any,
-		returnJson: boolean = false,
-		filePath: string = dataPath,
-		encoding: string = 'utf8'
-	) => {
-		fs.readFile(filePath, encoding, (err, data) => {
-			if (err) {
-				throw err;
-			}
+  const readFile = (
+    callback: (returnJson: boolean) => any,
+    returnJson: boolean = false,
+    filePath: string = dataPath,
+    encoding: string = 'utf8',
+  ) => {
+    fs.readFile(filePath, encoding, (err, data) => {
+      if (err) {
+        throw err;
+      }
 
-			callback(returnJson ? JSON.parse(data) : data);
-		});
-	};
+      callback(returnJson ? JSON.parse(data) : data);
+    });
+  };
 
-	const writeFile = (
-		fileData: string,
-		callback: () => void,
-		filePath = dataPath,
-		encoding = 'utf8'
-	) => {
-		fs.writeFile(filePath, fileData, encoding, (err) => {
-			if (err) {
-				throw err;
-			}
+  const writeFile = (
+    fileData: string,
+    callback: () => void,
+    filePath = dataPath,
+    encoding = 'utf8',
+  ) => {
+    fs.writeFile(filePath, fileData, encoding, (err) => {
+      if (err) {
+        throw err;
+      }
 
-			callback();
-		});
-	};
+      callback();
+    });
+  };
 
-	return {
-		readFile,
-		writeFile,
-	};
+  return {
+    readFile,
+    writeFile,
+  };
 };
