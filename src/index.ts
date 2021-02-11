1fc6082a3708356b065ad4e6504cbfcc59246ce5
Guy Roberts <guyroberts21@outlook.com>
2021-02-11 19:58:19 +0000

    Change export method

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,33 @@
 import * as fs from 'fs';
 
-export const utils = (dataPath: string) => {
-  const readFile = (
-    callback: (returnJson: boolean) => any,
-    returnJson: boolean = false,
-    filePath: string = dataPath,
-    encoding: string = 'utf8',
-  ) => {
-    fs.readFile(filePath, encoding, (err, data) => {
-      if (err) {
-        throw err;
-      }
+function readFile(
+  callback: (returnJson: boolean) => any,
+  returnJson: boolean = false,
+  filePath: string,
+  encoding: string = 'utf8',
+) {
+  fs.readFile(filePath, encoding, (err, data) => {
+    if (err) {
+      throw err;
+    }
 
-      callback(returnJson ? JSON.parse(data) : data);
-    });
-  };
+    callback(returnJson ? JSON.parse(data) : data);
+  });
+}
 
-  const writeFile = (
-    fileData: string,
-    callback: () => void,
-    filePath = dataPath,
-    encoding = 'utf8',
-  ) => {
-    fs.writeFile(filePath, fileData, encoding, (err) => {
-      if (err) {
-        throw err;
-      }
+function writeFile(
+  fileData: string,
+  callback: () => void,
+  filePath: string,
+  encoding = 'utf8',
+) {
+  fs.writeFile(filePath, fileData, encoding, (err) => {
+    if (err) {
+      throw err;
+    }
 
-      callback();
-    });
-  };
+    callback();
+  });
+}
 
-  return {
-    readFile,
-    writeFile,
-  };
-};
+export { readFile, writeFile };
